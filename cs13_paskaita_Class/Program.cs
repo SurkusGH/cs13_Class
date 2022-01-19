b40da59835fc2a81497854fa23e170518d330b01
Surkus <v.surkus@me.com>
2022-01-19 18:00:03 +0200

    Initial commit

---
+++ cs13_paskaita_Class/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace cs13_paskaita_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c13_PASKAITA_Class!");
+
+            #region TEORIJA - Klasės
+
+            //
+            //
+            //
+
+            #endregion
+
+            Problem1();
+            Problem2();
+            Problem3();
+            Problem4();
+
+        }
+
+        public static void Problem1()
+        {
+
+        }
+
+        public static void Problem2()
+        {
+
+        }
+
+        public static void Problem3()
+        {
+
+        }
+
+        public static void Problem4()
+        {
+
+        }
+    }
+}
+
